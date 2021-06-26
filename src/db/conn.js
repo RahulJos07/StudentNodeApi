13fea8b14a372472ba54ef6437ee2eae68f26b18
RahulJos07 <rj755871@gmail.com>
2021-06-26 15:17:06 +0530

    setup

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+require("dotenv").config();
 // //connect with mongodb with db name
 // mongoose.connect("mongodb://localhost:27017/students-api", {
 //     useCreateIndex: true,
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 // })
 
 //connect with mongodb atlas
-mongoose.connect("mongodb+srv://student_api:student_api@cluster0.sueqi.mongodb.net/StudentDB?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGO_URL, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
